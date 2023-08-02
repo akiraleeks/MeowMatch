70d89e96c8fdcc2a2c97d7f2e2f1226c70e836f7
aleeks2 <akira.leeks2@t-mobile.com>
2023-08-02 15:57:25 -0400

    Fix up cat details page layout

--- src/app/cat-details/cat-details.component.ts
+++ src/app/cat-details/cat-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICatBreedData } from '../landing/landing.component';
 
 @Component({
   selector: 'app-cat-details',
@@ -6,6 +7,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cat-details.component.scss']
 })
 export class CatDetailsComponent {
+  @Input() catDataDetails: ICatBreedData[];
   @Output() goBackAction: EventEmitter<string> = new EventEmitter();
 
   public goBack(): void {
