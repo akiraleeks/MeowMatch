d04a2e071897bd356fea04ddfe098c20ac143384
aleeks2 <akira.leeks2@t-mobile.com>
2023-08-02 23:51:14 -0400

    Update with cat search

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -9,10 +9,52 @@ export interface ICatBreedOptions {
   value?: string;
 }
 
+interface IOauthToken{
+  token_type: string,
+  expires_in: number,
+  access_token: string
+}
+
+export interface IAdopatbleAnimals{
+  animals: IAdoptaionDetails[]
+}
+
+interface IAdoptaionDetails{
+  age?: string;
+  gender?: string;
+  name?: string;
+  url?: string;
+  photos?: IAdoptationPhotos[];
+}
+
+interface IAdoptationPhotos{
+  full?: string;
+}
+
 export interface ICatBreedData{
-  name: string;
-  image: string;
-  description: string;
+  name?: string;
+  altName?: string;
+  image?: string;
+  description?: string;
+  weight?: string;
+  temperament?: string;
+  origin?: string;
+  lifespan?: string;
+  adaptability?: string,
+  affectionLevel?: string,
+  childFriendly?: string,
+  dogFriendly?: string,
+  energyLevel?: string,
+  grooming?: string,
+  healthIssues?: string,
+  sheddingLevel?: string,
+  socialNeeds?: string,
+  strangerFriendly?: string,
+  vocalisation?: string,
+  hairless?: string,
+  natural?: string,
+  rare?: string,
+  hypoallergenic?: string,
 }
 
 @Component({
@@ -29,7 +71,11 @@ export class LandingComponent {
   carBreedOptions: ICatBreedOptions[] = [];
   selectedCataData: ICatBreedData[] = [];
   showCard$: Observable<boolean>;
-  accessToken: string;
+  accessToken: IOauthToken;
+  activeTab: string = 'about';
+  zipCode: string;
+  adoptableCats: IAdopatbleAnimals;
+  hasError = false;
 
   @ViewChild('detailTemplate', { static: true }) public detailTemplate: TemplateRef<void>;
 
@@ -37,8 +83,7 @@ export class LandingComponent {
 
   async ngOnInit(): Promise<void> {
     await this.getCatBreedsData();
-    const tokenData = await RxjsUtil.firstValueFrom(this.apiService.getAccessToken());
-    console.log(tokenData);
+    this.accessToken = await RxjsUtil.firstValueFrom(this.apiService.getAccessToken());
   }
 
   public async getCatBreedsData(): Promise<void>{
@@ -56,11 +101,34 @@ export class LandingComponent {
     this.selectedCataData = catData.map((value) => {
       return {
         name: value.breeds[0].name,
+        altName: value.breeds[0].alt_names,
         image: value.url,
-        description: value.breeds[0].description
+        description: value.breeds[0].description,
+        weight: value.breeds[0].weight.metric,
+        temperament: value.breeds[0].temperament,
+        origin: value.breeds[0].origin,
+        lifespan: value.breeds[0].life_span,
+        adaptability: value.breeds[0].adaptability,
+        affectionLevel: value.breeds[0].affection_level,
+        childFriendly: value.breeds[0].child_friendly,
+        dogFriendly: value.breeds[0].dog_friendly,
+        energyLevel: value.breeds[0].energy_level,
+        grooming: value.breeds[0].grooming,
+        healthIssues: value.breeds[0].health_issues,
+        sheddingLevel: value.breeds[0].shedding_level,
+        socialNeeds: value.breeds[0].social_needs,
+        strangerFriendly: value.breeds[0].stranger_friendly,
+        vocalisation: value.breeds[0].vocalisation,
+        hairless: value.breeds[0].hairless,
+        natural: value.breeds[0].natural,
+        rare: value.breeds[0].rare,
+        hypoallergenic: value.breeds[0].hypoallergenic
       }
     });
     this.showCard$ = this.selectedCataData.length ? of(true) : of(false);
+    this.activeTab = 'about';
+    this.hasError = false;
+    this.adoptableCats = null;
   }
 
   public openCatDetails() {
@@ -70,5 +138,27 @@ export class LandingComponent {
   public async backFromDetail(): Promise<void> {
     this.detailAction.emit({showDetail: false});
     await RxjsUtil.sleepAsync(0);
+  }
+
+  public changeActiveTab(currentTab: string) {
+    this.activeTab = currentTab;
+    this.hasError = false;
+  }
+
+  public setZipCode(event) {
+    this.zipCode = event.target.value;
+  }
+
+  public async searchCatsByZip(): Promise<void>{
+    this.hasError = false;
+    try {
+      this.adoptableCats = await RxjsUtil.firstValueFrom(this.apiService.getCatsByZip(this.accessToken.access_token, this.selectedCataData[0].name, this.zipCode));
+      if (this.adoptableCats.animals.length === 0) {
+        this.hasError = true;
+      }
+    }
+    catch (error) {
+      this.hasError = true;
+    }
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IAdopatbleAnimals } from '../landing/landing.component';
 
 @Injectable({
   providedIn: 'root',
@@ -26,12 +27,19 @@ export class ApiService {
     );
   }
 
-  getAccessToken(): Observable<any[]> {
+  getAccessToken(): Observable<any> {
     const body = new HttpParams().set('grant_type', 'client_credentials')
       .set('client_id', this.client_id)
       .set('client_secret', this.client_secret);
-    return this.http.post<any[]>(
+    return this.http.post<any>(
       `${this.petFinderUrl}/oauth2/token`, body
+    );
+  }
+
+  getCatsByZip(accessToken: string, catBreed: string, zipCode: string): Observable<IAdopatbleAnimals> {
+    return this.http.get<IAdopatbleAnimals>(
+      `${this.petFinderUrl}/animals?type=cat&breed=${catBreed}&location=${zipCode}&page=1`,
+      { headers: new HttpHeaders().set('Authorization', `Bearer ${accessToken}`) }
     );
   }
 }
